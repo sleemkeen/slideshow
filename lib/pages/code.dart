330a4cd4a26b27cd948db1db37a5ed1409765d2a
slim <slim@slims-MBP.Dlink>
2020-09-27 14:14:50 +0100

    dropdown

--- lib/pages/code.dart
+++ lib/pages/code.dart
@@ -1,27 +1,103 @@
 import 'package:flutter/material.dart';
 class Code extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: Column(
-          children: [
-            Container(
-              height: 80,
-              color: Colors.green,
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.black,
+      child: Material(
+        child: Form(
+          key: _formKey,
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Color(0xFF171719),
+            child: Container(
+              child: Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 20),
+                    height: 80,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [Colors.black, Colors.transparent]
+                        )
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 40.0),
+                        Text(
+                          'Select your codes',
+                          style: TextStyle(
+                              color: Colors.white, fontSize: 25.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment(0, -0.8),
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      color: Color(0xFF171719),
+                      child: Theme(
+                        data: new ThemeData(
+                          primaryColor: Colors.green,
+                          primaryColorDark: Colors.green,
+                        ),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.green, style: BorderStyle.solid, width: 1.80),
+                          ),
+                          child: DropdownButton<String>(
+                            isExpanded: true,
+                            underline: SizedBox(),
+                            hint:  Text("Select your code", style: TextStyle(fontSize: 17.0, color: Colors.white),),
+                            items: <String>['A', 'B', 'C', 'D'].map((String value) {
+                              return new DropdownMenuItem<String>(
+                                value: value,
+                                child: new Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (_) {},
+                          ),
+                        )
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                    child: IntrinsicHeight(
+                      child: Container(
+                        height: 65,
+                        child: RaisedButton(
+                          color: Colors.green,
+                          onPressed: () {
+                            // Validate returns true if the form is valid, otherwise false.
+                            if (_formKey.currentState.validate()) {
+                              // If the form is valid, display a snackbar. In the real world,
+                              // you'd often call a server or save the information in a database.
+                              Scaffold
+                                  .of(context)
+                                  .showSnackBar(SnackBar(content: Text('Processing Data')));
+                            }
+                          },
+                          child: Text('Continue', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-            Container(
-              height: 80,
-              color:  Colors.orange,
-            )
-          ],
+          ),
         ),
       ),
     );
