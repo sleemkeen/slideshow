43cf5616b1fc2176173c3d95cba48940d78aef39
slim <slim@slims-MBP.Dlink>
2020-09-27 11:43:11 +0100

    ui structure

--- lib/pages/code.dart
+++ lib/pages/code.dart
@@ -2,7 +2,29 @@ import 'package:flutter/material.dart';
 class Code extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              height: 80,
+              color: Colors.green,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 80,
+              color:  Colors.orange,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
